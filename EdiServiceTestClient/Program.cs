9103426a28e023595fda30da5e4c6cfe9065d4fb
Steve Miller <Steve.Miller@ssigroup.com>
2020-08-25 18:21:14 -0500

    Add project files.

---
+++ EdiServiceTestClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using EdiService;
+using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace EdiServiceTestClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            var request = new HelloRequest() { Name = "Steve" };
+
+            var reply = await client.SayHelloAsync(request);
+
+            Console.WriteLine(reply.Message);
+
+            Console.ReadLine();
+        }
+    }
+}
