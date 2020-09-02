3ac76a107e07569aeec512c4ddfce951e37e7634
Steve Miller <smiller@crowninformationservices.com>
2020-09-02 16:34:17 -0500

    Basic input form built.

---
+++ Edi.Web/Models/EdiValidatorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Edi.Web.Models
+{
+    public class EdiValidatorViewModel
+    {
+        public string EdiRequest { get; set; }
+    }
+}
