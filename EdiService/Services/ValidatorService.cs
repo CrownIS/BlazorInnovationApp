5e3d671096a33f5829dce372ce45e05c51c1c8c5
Steve Miller <smiller@crowninformationservices.com>
2020-09-08 16:12:08 -0500

    Scaffold GRPC validator service.

---
+++ EdiService/Services/ValidatorService.cs
@@ -0,0 +1,29 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EdiService.Services
+{
+    public class ValidatorService : Validator.ValidatorBase
+    {
+        public ValidatorService(ILogger<ValidatorService> logger)
+        {
+            Logger = logger;
+        }
+
+        public ILogger<ValidatorService> Logger { get; }
+
+        public override Task<EdiValidationReply> ValidateEdi(EdiValidationRequest request, ServerCallContext context)
+        {
+            var output = new EdiValidationReply() 
+            {
+                Message = "It's all good!"
+            };
+
+            return Task.FromResult(output);
+        }
+    }
+}
--- EdiServiceTestClient/Program.cs
+++ EdiServiceTestClient/Program.cs
@@ -9,6 +9,26 @@ namespace EdiServiceTestClient
     {
         static async Task Main(string[] args)
         {
+            await GreeterClient();
+            await ValidatorClient();
+
+            Console.ReadLine();
+        }
+
+        private static async Task ValidatorClient()
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Validator.ValidatorClient(channel);
+
+            var request = new EdiValidationRequest() { Payload = "Edi to validate." };
+
+            var reply = await client.ValidateEdiAsync(request);
+
+            Console.WriteLine(reply.Message);
+        }
+
+        private static async Task GreeterClient()
+        {
             var channel = GrpcChannel.ForAddress("https://localhost:5001");
             var client = new Greeter.GreeterClient(channel);
 
@@ -17,8 +37,6 @@ namespace EdiServiceTestClient
             var reply = await client.SayHelloAsync(request);
 
             Console.WriteLine(reply.Message);
-
-            Console.ReadLine();
         }
     }
 }
