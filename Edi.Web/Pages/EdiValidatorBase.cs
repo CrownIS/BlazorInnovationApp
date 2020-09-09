7d2930db17b6f78cf8b46fdec38fb3ec11c67636
Steve Miller <smiller@crowninformationservices.com>
2020-09-09 08:39:22 -0500

    Wired up services. Seperated markup and code in razor component.

---
+++ Edi.Web/Pages/EdiValidatorBase.cs
@@ -0,0 +1,27 @@
+﻿using Edi.Web.Data;
+using Edi.Web.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Edi.Web.Pages
+{
+    public class EdiValidatorBase : ComponentBase
+    {
+        [Inject]
+        public EdiValidatorService ValidatorService { get; set; }
+
+        protected string Status = "Not submitted";
+        protected EdiValidatorViewModel ViewModel = new EdiValidatorViewModel();
+
+        protected async Task FormSubmitted()
+        {
+            var reply = await ValidatorService.Validate("Edi to validate.");
+            Debug.WriteLine("Clicked ...");
+            Status = $"Form submitted: {reply}";
+        }
+    }
+}
